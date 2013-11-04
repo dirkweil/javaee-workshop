c0ec77892fc52f38ea27093bee54132abce1aa5a
Dirk Weil <dirk.weil@gedoplan.de>
2013-11-04 12:20:21 +0100

    Editieren für SpecialDay erlaubt; Faces Flows; Transaktionsinterceptor / EJB

--- src/main/java/de/gedoplan/workshop/cman/gui/SpecialDayModel.java
+++ src/main/java/de/gedoplan/workshop/cman/gui/SpecialDayModel.java
@@ -1,31 +1,59 @@
 package de.gedoplan.workshop.cman.gui;
 
+import de.gedoplan.workshop.cman.persistence.All;
 import de.gedoplan.workshop.cman.persistence.SpecialDay;
 import de.gedoplan.workshop.cman.persistence.SpecialDayRepository;
 
+import java.io.Serializable;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+import javax.faces.flow.FlowScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 @Model
-public class SpecialDayModel
+@FlowScoped("specialDay")
+public class SpecialDayModel implements Serializable
 {
-  List<SpecialDay>     specialDays;
+  private static final long serialVersionUID = 1L;
 
   @Inject
-  SpecialDayRepository specialDayRepository;
+  @All
+  List<SpecialDay>          specialDays;
 
-  @PostConstruct
-  void postConstruct()
-  {
-    this.specialDays = this.specialDayRepository.findAll();
-  }
+  @Inject
+  SpecialDayRepository      specialDayRepository;
+
+  //
+  //  @PostConstruct
+  //  void postConstruct()
+  //  {
+  //    this.specialDays = this.specialDayRepository.findAll();
+  //  }
 
   public List<SpecialDay> getSpecialDays()
   {
     return this.specialDays;
   }
 
+  public void addSpecialDay()
+  {
+    this.specialDays.add(new SpecialDay());
+  }
+
+  // TODO: Ist das noch Präsentationslogik? In Service auslagern
+  @Transactional
+  public String save()
+  {
+    for (SpecialDay specialDay : this.specialDays)
+    {
+      if (specialDay.getId() != null && !specialDay.getId().isEmpty())
+      {
+        this.specialDayRepository.merge(specialDay);
+      }
+    }
+
+    return "home";
+  }
 }
--- src/main/java/de/gedoplan/workshop/cman/persistence/SpecialDayRepository.java
+++ src/main/java/de/gedoplan/workshop/cman/persistence/SpecialDayRepository.java
@@ -2,12 +2,17 @@ package de.gedoplan.workshop.cman.persistence;
 
 import java.util.List;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
 import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
-@ApplicationScoped
+//@ApplicationScoped
+//@Transactional()
+@Stateless
+@TransactionAttribute()
 public class SpecialDayRepository
 {
   @Inject
@@ -34,6 +39,8 @@ public class SpecialDayRepository
     return this.entityManager.find(SpecialDay.class, id);
   }
 
+  @Produces
+  @All
   public List<SpecialDay> findAll()
   {
     return this.entityManager.createQuery("select x from SpecialDay as x", SpecialDay.class).getResultList();
