25f9fa977d293cfb23631fdc58db28246cd456b0
Dirk Weil <dirk.weil@gedoplan.de>
2014-05-12 16:58:29 +0200

    REST

---
+++ src/main/java/de/gedoplan/workshop/webservice/RestApplication.java
@@ -0,0 +1,9 @@
+package de.gedoplan.workshop.webservice;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class RestApplication extends Application
+{
+}
---
+++ src/main/java/de/gedoplan/workshop/webservice/TalkResource.java
@@ -0,0 +1,34 @@
+package de.gedoplan.workshop.webservice;
+
+import de.gedoplan.workshop.domain.Talk;
+import de.gedoplan.workshop.persistence.TalkRepository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("talk")
+@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+public class TalkResource
+{
+  @Inject
+  TalkRepository talkRepository;
+
+  @GET
+  public List<Talk> getTalks()
+  {
+    return this.talkRepository.findAll();
+  }
+
+  @GET
+  @Path("{id}")
+  public Talk getTalk(@PathParam("id") Integer id)
+  {
+    return this.talkRepository.findById(id);
+  }
+}
