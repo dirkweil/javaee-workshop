65b1cd572ca7c2c69eae05dabc23ff2245aba3ea
Dirk Weil <dirk.weil@gedoplan.de>
2014-05-12 10:04:26 +0200

    Initiale Version mit SpecialDay

---
+++ src/main/java/de/gedoplan/workshop/domain/SpecialDay.java
@@ -0,0 +1,49 @@
+package de.gedoplan.workshop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class SpecialDay
+{
+  @Id
+  private String id;
+  private String name;
+
+  public SpecialDay(String id, String name)
+  {
+    this.id = id;
+    this.name = name;
+  }
+
+  public SpecialDay()
+  {
+  }
+
+  public String getId()
+  {
+    return this.id;
+  }
+
+  public void setId(String id)
+  {
+    this.id = id;
+  }
+
+  public String getName()
+  {
+    return this.name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  @Override
+  public String toString()
+  {
+    return "SpecialDay [id=" + this.id + ", name=" + this.name + "]";
+  }
+
+}
