5e7cb2f8f7abefcf5fe600d0aaa2bd2673d2bffc
Dirk Weil <dirk.weil@gedoplan.de>
2014-05-12 11:38:49 +0200

    Producer für EntityManager

--- src/main/java/de/gedoplan/workshop/persistence/SpecialDayRepository.java
+++ src/main/java/de/gedoplan/workshop/persistence/SpecialDayRepository.java
@@ -5,14 +5,15 @@ import de.gedoplan.workshop.domain.SpecialDay;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 @ApplicationScoped
 public class SpecialDayRepository
 {
-  @PersistenceContext(unitName = "default")
+  //  @PersistenceContext(unitName = "default")
+  @Inject
   EntityManager entityManager;
 
   public void persist(SpecialDay entity)
