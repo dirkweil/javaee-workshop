fec97d03d495857db378af37eac132bacf377fd6
Dirk Weil <dirk.weil@gedoplan.de>
2015-04-20 10:22:42 +0200

    Repository für SpecialDay hinzugefügt

---
+++ src/main/java/de/gedoplan/workshop/persistence/SpecialDayRepository.java
@@ -0,0 +1,41 @@
+package de.gedoplan.workshop.persistence;
+
+import de.gedoplan.workshop.domain.SpecialDay;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class SpecialDayRepository implements Serializable
+{
+  @PersistenceContext(unitName = "default")
+  EntityManager entityManager;
+
+  public void persist(SpecialDay specialDay)
+  {
+    this.entityManager.persist(specialDay);
+  }
+
+  public void merge(SpecialDay specialDay)
+  {
+    this.entityManager.merge(specialDay);
+  }
+
+  public void remove(SpecialDay specialDay)
+  {
+    this.entityManager.remove(specialDay);
+  }
+
+  public SpecialDay findById(String id)
+  {
+    return this.entityManager.find(SpecialDay.class, id);
+  }
+
+  public List<SpecialDay> findAll()
+  {
+    return this.entityManager.createQuery("select s from SpecialDay s", SpecialDay.class).getResultList();
+  }
+
+}
