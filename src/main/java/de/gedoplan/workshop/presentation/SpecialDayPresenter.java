98743fb8bd3cbedc5c54e5c841369dc3e2efcb5e
Dirk Weil <dirk.weil@gedoplan.de>
2015-11-02 11:47:45 +0100

    PostConstruct eingeführt

--- src/main/java/de/gedoplan/workshop/presentation/SpecialDayPresenter.java
+++ src/main/java/de/gedoplan/workshop/presentation/SpecialDayPresenter.java
@@ -5,6 +5,7 @@ import de.gedoplan.workshop.persistence.SpecialDayRepository;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 
@@ -14,9 +15,16 @@ public class SpecialDayPresenter
   @Inject
   SpecialDayRepository specialDayRepository;
 
+  List<SpecialDay>     specialDays;
+
   public List<SpecialDay> getSpecialDays()
   {
-    return this.specialDayRepository.findAll();
+    return this.specialDays;
   }
 
+  @PostConstruct
+  private void init()
+  {
+    this.specialDays = this.specialDayRepository.findAll();
+  }
 }
