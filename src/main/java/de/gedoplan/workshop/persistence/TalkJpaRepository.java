c843849ea0a7d9d1dcb34ef34aceebb8e7d6443f
Dirk Weil <dirk.weil@gedoplan.de>
2016-11-07 14:52:08 +0100

    Detail-Dialog für Talk TX-Steuerung

--- src/main/java/de/gedoplan/workshop/persistence/TalkJpaRepository.java
+++ src/main/java/de/gedoplan/workshop/persistence/TalkJpaRepository.java
@@ -4,7 +4,9 @@ import de.gedoplan.baselibs.persistence.repository.SingleIdEntityRepository;
 import de.gedoplan.workshop.entity.Talk;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
+@Transactional(rollbackOn = Exception.class)
 public class TalkJpaRepository extends SingleIdEntityRepository<Integer, Talk> implements TalkRepository {
 }
--- src/main/java/de/gedoplan/workshop/presentation/TalkPresenter.java
+++ src/main/java/de/gedoplan/workshop/presentation/TalkPresenter.java
@@ -3,24 +3,40 @@ package de.gedoplan.workshop.presentation;
 import de.gedoplan.workshop.entity.Talk;
 import de.gedoplan.workshop.persistence.TalkRepository;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import lombok.Getter;
+
 @Named
-@RequestScoped
-public class TalkPresenter {
+@SessionScoped
+public class TalkPresenter implements Serializable {
 
   @Inject
   TalkRepository talkRepository;
 
   private List<Talk> talks;
 
+  @Getter
+  private Talk currentTalk;
+
   public List<Talk> getTalks() {
     return this.talks;
+  }
+
+  public String editTalk(Talk talk) {
+    this.currentTalk = talk;
+    return "editTalk";
+  }
+
+  public String saveTalk() {
+    this.talkRepository.merge(this.currentTalk);
+    return "talk";
   }
 
   @PostConstruct
