a8562da8a5fb6144c498eaa1129cf0d335487f7b
Dirk Weil <dirk.weil@gedoplan.de>
2016-11-07 16:46:50 +0100

    REST: PUT / POST

--- src/main/java/de/gedoplan/workshop/webservice/TalkResource.java
+++ src/main/java/de/gedoplan/workshop/webservice/TalkResource.java
@@ -3,18 +3,26 @@ package de.gedoplan.workshop.webservice;
 import de.gedoplan.workshop.entity.Talk;
 import de.gedoplan.workshop.persistence.TalkRepository;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 @Path("talk")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class TalkResource {
   @Inject
   TalkRepository talkRepository;
@@ -34,5 +42,24 @@ public class TalkResource {
 
     throw new NotFoundException();
     // throw new WebApplicationException(42);
+  }
+
+  @PUT
+  @Path("{hugo}")
+  public void putById(@PathParam("hugo") Integer id, Talk talk) {
+    Talk oldTalk = this.talkRepository.findById(id);
+    if (oldTalk == null) {
+      throw new NotFoundException();
+    }
+
+    this.talkRepository.merge(talk);
+  }
+
+  @POST
+  public Response create(Talk talk, @Context UriInfo uriInfo) {
+    this.talkRepository.persist(talk);
+
+    URI location = uriInfo.getAbsolutePathBuilder().path(talk.getId().toString()).build();
+    return Response.created(location).build();
   }
 }
