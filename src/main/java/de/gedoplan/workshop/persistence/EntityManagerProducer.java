c4bc20a1ae0168b52ade902beb45065f72f99e01
Dirk Weil <dirk.weil@gedoplan.de>
2017-05-08 16:07:39 +0200

    Extended EM

--- src/main/java/de/gedoplan/workshop/persistence/EntityManagerProducer.java
+++ src/main/java/de/gedoplan/workshop/persistence/EntityManagerProducer.java
@@ -1,17 +1,22 @@
 package de.gedoplan.workshop.persistence;
 
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 
+@RequestScoped
+@Stateful
 public class EntityManagerProducer {
-  @PersistenceContext(unitName = "default")
-  @Produces
-  EntityManager entityManager;
+  @PersistenceContext(unitName = "default", type = PersistenceContextType.EXTENDED)
+  private EntityManager entityManager;
 
-  // @Produces
-  // public EntityManager getEntityManager() {
-  // return this.entityManager;
-  // }
+  @Produces
+  @RequestScoped
+  public EntityManager getEntityManager() {
+    return this.entityManager;
+  }
 
 }
