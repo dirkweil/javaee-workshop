bdeefdc7bf850f6968feb13352f8bceda09953b8
Dirk Weil <dirk.weil@gedoplan.de>
2017-05-08 17:55:53 +0200

    Producer für fachliche Injektion

--- src/main/java/de/gedoplan/workshop/persistence/impl/PersonJpaRepository.java
+++ src/main/java/de/gedoplan/workshop/persistence/impl/PersonJpaRepository.java
@@ -4,9 +4,19 @@ import de.gedoplan.baselibs.persistence.repository.SingleIdEntityRepository;
 import de.gedoplan.workshop.domain.Person;
 import de.gedoplan.workshop.persistence.PersonRepository;
 
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
 
 @ApplicationScoped
 public class PersonJpaRepository extends SingleIdEntityRepository<Integer, Person> implements PersonRepository {
+
+  @Produces
+  @Override
+  public List<Person> findAll() {
+    return super.findAll();
+  }
   // evtl. weitere Finder
+
 }
