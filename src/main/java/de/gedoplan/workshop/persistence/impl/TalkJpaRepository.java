405b3af455086fe3fee94798cac5941239544d8a
Dirk Weil <dirk.weil@gedoplan.de>
2017-05-08 14:08:26 +0200

    Producer, Repo-Basisklassen

--- src/main/java/de/gedoplan/workshop/persistence/impl/TalkJpaRepository.java
+++ src/main/java/de/gedoplan/workshop/persistence/impl/TalkJpaRepository.java
@@ -1,36 +1,12 @@
 package de.gedoplan.workshop.persistence.impl;
 
+import de.gedoplan.baselibs.persistence.repository.SingleIdEntityRepository;
 import de.gedoplan.workshop.domain.Talk;
 import de.gedoplan.workshop.persistence.TalkRepository;
 
-import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-public class TalkJpaRepository implements TalkRepository {
-
-  @PersistenceContext(unitName = "default")
-  EntityManager entityManager;
-
-  @Override
-  public void persist(Talk talk) {
-    this.entityManager.persist(talk);
-  }
-
-  @Override
-  public Talk merge(Talk talk) {
-    return this.entityManager.merge(talk);
-  }
-
-  @Override
-  public Talk findById(Integer id) {
-    return this.entityManager.find(Talk.class, id);
-  }
-
-  @Override
-  public List<Talk> findAll() {
-    return this.entityManager.createQuery("select t from Talk t", Talk.class).getResultList();
-  }
-
+@ApplicationScoped
+public class TalkJpaRepository extends SingleIdEntityRepository<Integer, Talk> implements TalkRepository {
+  // evtl. weitere Finder
 }
