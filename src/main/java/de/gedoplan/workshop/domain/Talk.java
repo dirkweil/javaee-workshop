f168b9ceadfa4594dc3ef42ebbc6c9dac651f3b6
Dirk Weil <dirk.weil@gedoplan.de>
2017-05-08 17:56:43 +0200

    Validierung mit Bean Validation

--- src/main/java/de/gedoplan/workshop/domain/Talk.java
+++ src/main/java/de/gedoplan/workshop/domain/Talk.java
@@ -17,6 +17,10 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -28,11 +32,20 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Talk extends GeneratedIntegerIdEntity {
+  @NotNull
+  @Size(min = 10, message = "Bitte min. 10 Zeichen")
+  @Pattern(regexp = "[A-Z].*")
   private String title;
 
+  @NotNull
   @Column(name = "TALK_TYPE")
   @Enumerated(EnumType.STRING)
   private TalkType type;
+
+  @AssertTrue(message = "Keynotes brauchen lange Titel (min 40 Zeichen)")
+  private boolean isValid() {
+    return !this.type.equals(TalkType.KEYNOTE) || this.title.length() > 40;
+  }
 
   @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(joinColumns = @JoinColumn(name = "TALK_ID"), inverseJoinColumns = @JoinColumn(name = "SPEAKER_ID"))
--- src/main/java/de/gedoplan/workshop/presentation/TalkPresenter.java
+++ src/main/java/de/gedoplan/workshop/presentation/TalkPresenter.java
@@ -1,20 +1,27 @@
 package de.gedoplan.workshop.presentation;
 
+import de.gedoplan.workshop.domain.Person;
 import de.gedoplan.workshop.domain.Talk;
 import de.gedoplan.workshop.domain.TalkType;
 import de.gedoplan.workshop.persistence.TalkRepository;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 
 import org.apache.deltaspike.core.api.scope.ViewAccessScoped;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Named // ("talkPresenter")
 // @SessionScoped
@@ -53,7 +60,24 @@ public class TalkPresenter implements Serializable {
     return "editTalk";
   }
 
+  @Inject
+  Validator validator;
+
   public String saveTalk() {
+    Set<ConstraintViolation<Talk>> errors = this.validator.validate(this.currentTalk);
+    if (!errors.isEmpty()) {
+      // Das Objekt ist nicht valide; man könnte eine spezifische Fehlermeldung
+      // erzeugen, aber
+      // das sei dem Leser als Übung überlassen ;-) (Hinweis: API von
+      // ConstraintViolation)
+      FacesMessage message = new FacesMessage("Fehler: " + errors);
+      message.setSeverity(FacesMessage.SEVERITY_ERROR);
+      FacesContext.getCurrentInstance().addMessage(null, message);
+
+      // auf Seite verbleiben
+      return null;
+    }
+
     this.talkRepository.merge(this.currentTalk);
 
     postConstruct();
@@ -64,4 +88,17 @@ public class TalkPresenter implements Serializable {
   public TalkType[] getTalkTypes() {
     return TalkType.values();
   }
+
+  @Getter
+  @Inject
+  List<Person> allSpeakers;
+
+  @Getter
+  @Setter
+  Person additionalSpeaker;
+
+  public void addSpeaker() {
+    this.currentTalk.getSpeakers().add(this.additionalSpeaker);
+  }
+
 }
