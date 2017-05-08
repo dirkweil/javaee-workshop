067623807c158f0c9799c06319e74ef3d033905d
Dirk Weil <dirk.weil@gedoplan.de>
2017-05-08 15:44:36 +0200

    JPA-Relationen

--- src/main/java/de/gedoplan/workshop/domain/Talk.java
+++ src/main/java/de/gedoplan/workshop/domain/Talk.java
@@ -2,15 +2,23 @@ package de.gedoplan.workshop.domain;
 
 import de.gedoplan.baselibs.persistence.entity.GeneratedIntegerIdEntity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,12 +34,18 @@ public class Talk extends GeneratedIntegerIdEntity {
   @Enumerated(EnumType.STRING)
   private TalkType type;
 
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(joinColumns = @JoinColumn(name = "TALK_ID"), inverseJoinColumns = @JoinColumn(name = "SPEAKER_ID"))
+  @Setter(AccessLevel.NONE)
+  private Set<Person> speakers;
+
   @Transient
   private int dummy;
 
   public Talk(String title, TalkType type) {
     this.title = title;
     this.type = type;
+    this.speakers = new HashSet<>();
   }
 
   protected Talk() {
